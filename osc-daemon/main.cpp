3393edccfe3066e95f989e2eefbeb8a3a7c3bf1f
Aidan Macrae <2306781m@student.gla.ac.uk>
2022-11-10 15:06:02 +0000

    first draft of ASAT code to be fixed later lol

--- osc-daemon/main.cpp
+++ osc-daemon/main.cpp
@@ -50,3 +50,82 @@ double t_burnout;
 //LRCT = (find(h_c>100000, 1)-1);                 //first time index where culmination height is above 100km
 //ASAT_AltAtLRCT=h(LRCT);                         //Altitude of ASAT-M at Low Risk Critical Time
 }
+
+//get ASAT positions and velocities from following equations
+//deltaM = (ASATflightTime - ReactionTime)*MeanOrbitalMotion(SatKOE)
+//deltaE = MeanToTrue(deltaM, SatKOE)
+//ASATp = [0, deltaE, h]
+//ASATv = [V*cos(deltaE), V*sin(deltaE), 0]
+
+//set up big fucking nasty array
+//every combination between two orthogonal sets of deltaVs
+
+//set up bigger fucking nastier array
+//BFNA by 13 array
+
+//for each combination
+    //perform burn of combination deltaV (Burn)
+    //find intercept values (InterceptCalcs)
+    //find if intercept is exo/endo atmospheric and viable
+        //some vile fucking logic here
+        //save any viable intercepts, ignore the rest
+
+//find lowest deltaV burn for both exo and atmo
+
+//do SMAD calculations for Casat
+//plot graphs
+
+// function [CaSatInterceptTime, CaSatAltAtTheta, ASAT_AltAtIntercept] = InterceptCalcs(KOE)
+//     global deltaM mu EarthRadius ModelAccuracy ReactionTime h
+//     %CASAT altitude equations
+//     [dTheta,~]=MeanToTrue(deltaM, KOE(2));
+//     if KOE(6)+dTheta>=360
+//        dTheta=dTheta-360;
+//     end
+//     Function=@(v) 1./(1+KOE(2).*cosd(v)).^2;
+//     if KOE(2)>0 && KOE(2)<1
+//     [r, V] = keplerian2ijk(KOE(1), KOE(2), KOE(3), KOE(4), KOE(5), KOE(6)+dTheta, 'lonper', KOE(6)+dTheta);
+//     elseif KOE(2)==0
+//     [r, V] = keplerian2ijk(KOE(1), KOE(2), KOE(3), KOE(4), KOE(5), KOE(6)+dTheta, 'truelon', KOE(6)+dTheta);
+//     else
+//         CaSatInterceptTime=99999999;
+//         CaSatAltAtTheta=999999999;
+//         ASAT_AltAtIntercept=99999999;
+//         return
+//     end
+//     AngularMomentum=norm(cross(r, V));
+//     if KOE(6)>315 && KOE(6)+dTheta<45
+//         dTheta=dTheta+360;
+//     end
+//     CaSatInterceptTime=abs(integral(Function, deg2rad(KOE(6)), deg2rad(KOE(6)+dTheta)))/(mu^2/AngularMomentum^3);
+//     CaSatAltAtTheta=norm(r)-EarthRadius;
+//     if ReactionTime/ModelAccuracy+(round(CaSatInterceptTime/ModelAccuracy))<=1000000
+//         ASAT_AltAtIntercept=h(ReactionTime/ModelAccuracy+(round(CaSatInterceptTime/ModelAccuracy)));   
+//     else
+//         ASAT_AltAtIntercept=10^9;
+//     end
+    
+// end
+
+function KOE = Burn(DeltaVvnb, KOE)%burns by a [dVr dVt dVn]m/s impulse burn
+    if KOE(2)==0
+        [r, t] = keplerian2ijk(KOE(1), KOE(2), KOE(3), KOE(4), KOE(5), KOE(6), 'truelon', KOE(6));
+    else
+        [r, t] = keplerian2ijk(KOE(1), KOE(2), KOE(3), KOE(4), KOE(5), KOE(6), 'lonper', KOE(6));
+    end
+    vnb2ijk=[t./norm(t) cross(r,t)./norm(cross(r,t)) cross(t,cross(r,t))./norm(cross(t,cross(r,t)))];
+    DeltaVijk=vnb2ijk*DeltaVvnb';
+    t=t+DeltaVijk;
+    [KOE(1), KOE(2), KOE(3), KOE(4), KOE(5), KOE(6), trulon]=ijk2keplerian(r, t);
+    if ~isfinite(KOE(6))==1
+        KOE(6)=trulon;
+    end
+    if ~isfinite(KOE(4))==1 || ~isfinite(KOE(5))==1
+    KOE(4)=0; KOE(5)=0;
+    end
+end
+
+function n = MeanOrbitalMotion(KOE)
+global mu
+    n = rad2deg(sqrt(mu/(KOE(1))^3));
+end
